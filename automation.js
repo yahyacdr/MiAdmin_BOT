9683d4df619793b934d7d184683f1595850bb45f
yahyacdr <yahya.zahouani7@gmail.com>
2024-01-04 15:59:10 +0100

    inserting multiple items succussfully

--- automation.js
+++ automation.js
@@ -1,6 +1,7 @@
 const { Builder, Browser, By, Key, until } = require('selenium-webdriver');
 
 (async function example() {
+    let item_list = ['New product 1', 'New product 2']
     let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
     try {
 
@@ -17,39 +18,59 @@ const { Builder, Browser, By, Key, until } = require('selenium-webdriver');
         await driver.sleep(2000)
 
         // MOVE TO THE LAST ELEMENT
-        await driver.findElement(By.css('.k-link.k-pager-nav.k-pager-last')).click()
+        async function insert_data(data, i) {
+            await goToLastPage()
 
-        // SCROLL LAST ELEMENT TO VIEW
-        const ele = await driver.findElement(By.xpath('/html/body/div[2]/div[2]/div[5]/div/div[2]/table/tbody/tr[1]/td[2]/div/div[3]/div/div[3]/table/tbody/tr[last()]'))
-        await driver.executeScript("arguments[0].scrollIntoView(true);", ele);
+            await insertItem(data, i)
 
-        await driver.sleep(5000)
+        }
 
-        await driver.executeScript("arguments[0].click();", await driver.findElement(By.xpath('/html/body/div[2]/div[2]/div[5]/div/div[2]/table/tbody/tr[1]/td[2]/div/div[3]/div/div[3]/table/tbody/tr[last()]/td[1]/input')));
-        await driver.sleep(500)
-        await driver.executeScript("arguments[0].dispatchEvent(new MouseEvent('mousedown', { bubbles: true, cancelable: true, view: window }));", await driver.findElement(By.css('#MasterGrid > div.k-grid-content > table > tbody > tr:last-child')));
-        await driver.executeScript("arguments[0].dispatchEvent(new MouseEvent('mouseup', { bubbles: true, cancelable: true, view: window }));", await driver.findElement(By.css('#MasterGrid > div.k-grid-content > table > tbody > tr:last-child')));
+        async function goToLastPage() {
+            await driver.findElement(By.css('.k-link.k-pager-nav.k-pager-last')).click()
+            // SCROLL LAST ELEMENT TO VIEW
+            const ele = await driver.findElement(By.xpath('/html/body/div[2]/div[2]/div[5]/div/div[2]/table/tbody/tr[1]/td[2]/div/div[3]/div/div[3]/table/tbody/tr[last()]'))
+            await driver.executeScript("arguments[0].scrollIntoView(true);", ele);
+            await driver.sleep(5000)
 
-        await driver.executeScript("arguments[0].click();", await driver.findElement(By.id('toDuplicate')));
+            await driver.executeScript("arguments[0].click();", await driver.findElement(By.xpath('/html/body/div[2]/div[2]/div[5]/div/div[2]/table/tbody/tr[1]/td[2]/div/div[3]/div/div[3]/table/tbody/tr[last()]/td[1]/input')));
 
-        await driver.sleep(3000)
+            await driver.sleep(500)
+            await driver.executeScript("arguments[0].dispatchEvent(new MouseEvent('mousedown', { bubbles: true, cancelable: true, view: window }));", await driver.findElement(By.css('#MasterGrid > div.k-grid-content > table > tbody > tr:last-child')));
+            await driver.executeScript("arguments[0].dispatchEvent(new MouseEvent('mouseup', { bubbles: true, cancelable: true, view: window }));", await driver.findElement(By.css('#MasterGrid > div.k-grid-content > table > tbody > tr:last-child')));
 
-        // ENTERING DATA
-        let scriptCode = `arguments[0].value = 'D' + 1002`
-        await driver.executeScript(scriptCode, await driver.findElement(By.id('ItemCode')))
-        let scriptNameA = `arguments[0].value = 'New Product'`
-        await driver.executeScript(scriptNameA, await driver.findElement(By.id('ItemNameA')))
-        let scriptNameL = `arguments[0].value = 'New Product'`
-        await driver.executeScript(scriptNameL, await driver.findElement(By.id('ItemNameL')))
+            await driver.executeScript("arguments[0].click();", await driver.findElement(By.id('toDuplicate')));
 
-        await driver.executeScript("arguments[0].value = ''", await driver.findElement(By.id('PortalDisplayName')))
-        await driver.executeScript("arguments[0].value = ''", await driver.findElement(By.id('PortalDisplayLName')))
-        await driver.executeScript("arguments[0].value = ''", await driver.findElement(By.id('PartNumber')))
+            await driver.sleep(3000)
+        }
 
-        await driver.findElement(By.xpath('/html/body/div[30]/div[2]/form/div[1]/input')).click()
-        await driver.sleep(2000)
-        await driver.findElement(By.id('popup_ok')).click()
-        await driver.findElement(By.css('button.ui-button:nth-child(1)')).click()
+        async function insertItem(data, i) {
+            // ENTERING DATA
+            let scriptCode = `arguments[0].value = 'D${1004 + i}'`
+            await driver.executeScript(scriptCode, await driver.findElement(By.id('ItemCode')))
+            let scriptNameA = `arguments[0].value = '${data}'`
+            await driver.executeScript(scriptNameA, await driver.findElement(By.id('ItemNameA')))
+            let scriptNameL = `arguments[0].value = '${data}'`
+            await driver.executeScript(scriptNameL, await driver.findElement(By.id('ItemNameL')))
+
+            await driver.executeScript("arguments[0].value = ''", await driver.findElement(By.id('PortalDisplayName')))
+            await driver.executeScript("arguments[0].value = ''", await driver.findElement(By.id('PortalDisplayLName')))
+            await driver.executeScript("arguments[0].value = ''", await driver.findElement(By.id('PartNumber')))
+
+            const ele = await driver.findElement(By.css('.PublicDiv'))
+            await driver.executeScript("arguments[0].scrollIntoView(true);", ele);
+
+            await driver.findElement(By.css('.PublicDiv')).click()
+            await driver.sleep(2000)
+            await driver.findElement(By.id('popup_ok')).click()
+            await driver.findElement(By.css('button.ui-button:nth-child(1)')).click()
+
+        }
+
+
+        for (let [i, item] of item_list.entries()) {
+            await insert_data(item, i)
+        }
+
         // await driver.findElement(By.id('toDuplicate')).click()
         // await ele.click()
         // await driver.wait(until.elementLocated(driver.findElement(By.css('.k-widget.k-dropdown.k-header'))), 1000);
